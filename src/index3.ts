6a301a54f12a57d6e30ffce41d6fa28eed4a0b93
Ayaka <KiritaniAyaka@users.noreply.github.com>
2022-10-11 13:21:27 +0800

    add case

---
+++ src/index3.ts
@@ -0,0 +1,19 @@
+export default function (a: number) {
+	switch (a) {
+		case 1:
+			console.log(1);
+			break;
+
+		case 2:
+			console.log(2);
+			break;
+
+		case 3:
+			console.log(3);
+			break;
+
+		default:
+			console.log('default');
+	}
+	return a;
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,13 +1,18 @@
 import func from "../src/index";
 import func2 from "../src/index2";
+import func3 from "../src/index3";
 import { it } from "mocha";
 
 it("index", () => {
-  func(true);
-  func(false);
+	func(true);
+	func(false);
 });
 
 it("index2", () => {
-  func2(true);
-  func2(false);
+	func2(true);
+	func2(false);
 });
+
+it("index3", () => {
+	func3(1);
+})
